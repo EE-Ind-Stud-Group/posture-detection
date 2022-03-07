2793735e1dc1c8193df4f0e4c6d9c97ac427a14b
Lai-YT <381xvmvbib@gmail.com>
2022-03-08 00:24:56 +0800

    feat: evaluate model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Union
 from pathlib import Path
 
 import numpy as np
@@ -8,11 +9,11 @@ from tensorflow.keras import models, utils
 from train import PostureLabel
 
 
-def main() -> None:
-    model = models.load_model(Path.cwd() / "model")
-    image = utils.load_img(
-        Path.cwd() / "posture/samples/good/0001.jpg"
-    )
+model = models.load_model(Path.cwd() / "model")
+
+
+def predict_single_image(imagepath: Union[Path, str]) -> None:
+    image = utils.load_img(Path.cwd() / imagepath)
     image_array = utils.img_to_array(image)
     image_array = tf.expand_dims(image_array, 0)
 
@@ -22,6 +23,34 @@ def main() -> None:
         f"This image most likely belongs to {PostureLabel(np.argmax(score)).name} "
         f"with a {np.max(score):.2%} percent confidence."
     )
+
+
+def predict_images(folderpath: Union[Path, str]) -> None:
+    test_ds = utils.image_dataset_from_directory(
+        Path.cwd() / folderpath,
+        shuffle=False,
+        image_size=(480, 640),
+        batch_size=15
+    )
+    predictions = model.predict(test_ds)
+    scores = tf.nn.softmax(predictions)
+    for score in scores:
+        print(PostureLabel(np.argmax(score)).name)
+
+
+def evaluate_images(folderpath: Union[Path, str]) -> None:
+    eval_ds = utils.image_dataset_from_directory(
+        Path.cwd() / folderpath,
+        seed=123,
+        image_size=(480, 640),
+        batch_size=15
+    )
+    model.evaluate(eval_ds)
+
+
+def main() -> None:
+    # predict_single_image("posture/samples/good/0001.jpg")
+    evaluate_images("posture/test")
 
 
 if __name__ == "__main__":
